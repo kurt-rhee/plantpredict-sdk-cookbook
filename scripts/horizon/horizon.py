046fe885eb2c845d7138356892a0e61e76147250
Kurt Rhee <33131958+kurt-rhee@users.noreply.github.com>
2024-08-06 11:58:12 -0700

    horizon details editing

--- scripts/horizon/horizon.py
+++ scripts/horizon/horizon.py
@@ -9,11 +9,22 @@ path_to_folder = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath
 sys.path.insert(0, path_to_folder)
 from scripts.authentication.authentication import authenticate
 
+# authenticate
 api = authenticate()
+
+# put prediction into a python object
 prediction = api.prediction(
     project_id=136798,
     id=757745
 )
 
+# Get the horizon from the prediction in list of dictionaries format
 horizon = prediction.get()['horizon_details']
-print(horizon)
+
+# Edit the first entry by changing the elevation to another number
+horizon_edited = horizon.copy()
+horizon_edited[0]['elevation'] = 10.0
+prediction.horizon_details = horizon_edited
+
+# Send update
+prediction.update()
